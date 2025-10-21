f0bdd346c82febf4706975c2d32d5f3d07134c18
Oskar-Kowalski <oskar.kowalski09@gmail.com>
2025-10-21 13:07:27 +0200

    Dodanie formularza napiwków

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-// function calculateTip() {
-//     // Kod do implementacji
-// }
+const form = document.getElementById("form");
 
-// ... Kod do implementacji
+form.addEventListener("submit", calculateTip);
+
+function calculateTip(event) {
+  event.preventDefault();
+  const data = new FormData(event.target);
+
+  const rachunek = data.get("rachunek");
+  const napiwek = data.get("napiwek");
+
+  const wynik = document.getElementById("wynik");
+
+  const liczba1 = parseFloat(rachunek);
+  const liczba2 = parseFloat(napiwek);
+
+  const procentNapiwku = liczba2 / 100;
+  const kwotanapiwku = liczba1 * procentNapiwku;
+
+  const calkwota = liczba1 + kwotanapiwku;
+
+  wynik.innerText = `Kwota napiwku: ${kwotanapiwku.toFixed(
+    2
+  )}, całkowita kwota: ${calkwota.toFixed(2)} `;
+}
